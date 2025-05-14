bcd62eecb18523ebf0bc80ed96ef0566d54780f0
Tyson Brown <thetysonbrown@gmail.com>
2025-05-14 14:52:36 -0700

    data and training script complete for now

---
+++ server/train.py
@@ -0,0 +1,29 @@
+from ultralytics import YOLO
+import torch
+
+def main():
+    if torch.cuda.is_available():
+        device = 0
+        print("Using GPU for training.")
+    else:
+        device = 'cpu'
+        print("Using CPU for training.")
+
+    dataset_yaml = 'data.yaml'
+
+    model = YOLO('yolov8n.pt')
+
+    model.train(
+        data=dataset_yaml,
+        epochs=50,
+        imgsz=640,
+        batch=16,
+        workers=4,
+        device=device,
+        project='server'
+    )
+
+    model.val()
+
+if __name__ == "__main__":
+    main()
