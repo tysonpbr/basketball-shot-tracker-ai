17a78a19d91e4bb5ba7c40f0d91186261f3107d5
Tyson Brown <thetysonbrown@gmail.com>
2025-05-22 14:45:07 -0700

    added icons

---
+++ client/src/assets/icons/index.js
@@ -0,0 +1,11 @@
+import icon_account from './icon_account.png'
+import icon_camera from './icon_camera.png'
+import icon_home from './icon_home.png'
+import icon_settings from './icon_settings.png'
+
+export {
+  icon_account,
+  icon_camera,
+  icon_home,
+  icon_settings
+}
